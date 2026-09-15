ab98bbfc3fc3d539d2adb7eccefeaf85cb5b0ac7
Atul Pandey <atulpandey.iitr@gmail.com>
2026-09-15 12:54:19 +0530

    Initialize project structure

---
+++ main.cpp
@@ -0,0 +1,8 @@
+#include <iostream>
+
+int main() {
+    std::cout << "FARM_Farm_to_Market_Supply_Optimization_Network" << std::endl;
+    std::cout << "Project initialized successfully." << std::endl;
+
+    return 0;
+}
